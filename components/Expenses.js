eef98d19ffe957f7f8c711b0a81ed57765d89548
Vikrant Katoch <vikrantkatoch30@gmail.com>
2021-05-14 16:32:35 +0530

    Update Expenses.js

--- components/Expenses.js
+++ components/Expenses.js
@@ -1,14 +1,32 @@
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css"
+import { useState } from "react";
+
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const saveSelectedYear = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpensesList = props.items.filter((expense) => {
+    // getFullYear returns the date in a number format, but the filtered year is string
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <div class="expenses">
-      <ExpenseItem
-        title={props.title}
-        amount={props.amount}
-        date={props.date}
-      />
+    <div>
+      <div className="expenses">
+        <ExpensesFilter
+          activeYear={filteredYear}
+          onSelectedYear={saveSelectedYear}
+        />
+        <ExpensesChart expenses={filteredExpensesList}/>
+        <ExpensesList items={filteredExpensesList} />
+      </div>
     </div>
   );
 }
