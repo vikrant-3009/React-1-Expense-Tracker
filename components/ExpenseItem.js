4c24609d3f7c477c884e066c8061960f10588555
Vikrant Katoch <vikrantkatoch30@gmail.com>
2021-05-14 16:33:43 +0530

    Update ExpenseItem.js

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,16 +1,29 @@
+// import { useState } from "react"
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 // "Props" is basically an attribute through which we can pass objects & it's propperties to custom components
 function ExpenseItem(props) {
+  // const [title, setTitle] = useState(props.title);
+
+  // Click Handler Function
+  // const clickHandler = () => {
+  //   setTitle("Updated");
+  //   console.log(title);
+  // };
+
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">Rs {props.amount}/-</div>
+    <li>
+      <div className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">Rs {props.amount}/-</div>
+        </div>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </div>
-    </div>
+    </li>
   );
 }
 
